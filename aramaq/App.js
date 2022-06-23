c57bbad5b8ef0c3d155942b6c8bfed82cf082127
Rafael Alcântara <devrafasouza@gmail.com>
2022-06-23 10:48:25 -0300

    tela de serviços

--- aramaq/App.js
+++ aramaq/App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { 
+  StatusBar, 
+  SafeAreaView,
+  View 
+} from 'react-native';
+import { 
+  useFonts,
+  Montserrat_400Regular, 
+  Montserrat_700Bold 
+} from '@expo-google-fonts/montserrat';
+
+import Servicos from './src/screens/Servicos/';
+import mock from './src/mocks/servicos.js';
 
 export default function App() {
+  const [fontCarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold
+  })
+
+  if(!fontCarregada) {
+    return <View />
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1}} >
+      <StatusBar barStyle="dark-content"/>
+      <Servicos {...mock} />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
